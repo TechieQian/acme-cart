4cdc0a88926221b407b445dfbc9549804eaf2eec
Qian Wan <qiansan@gmail.com>
2017-08-27 15:26:38 -0400

    update

--- db/conn.js
+++ db/conn.js
@@ -1,4 +1,4 @@
 const Sequelize = require('sequelize')
-const conn = new Sequelize('postgres://localhost:5432/cart_db')
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/cart_db')
 
 module.exports = conn
